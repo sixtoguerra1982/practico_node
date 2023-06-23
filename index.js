861e9af7550d0fe66c1212339039d175d5300c43
= <=>
2023-06-22 22:39:13 -0400

    last version

--- index.js
+++ index.js
@@ -1,36 +1,71 @@
-// 1) RECIBIR LOS DATOS POR ARGUMENTO
-// 2) DIVIDIR Y VALIDAR LOS DATOS
-// 3) API https://mindicador.cl/api
-
+// CARGAR BIBLIOTECAS
 const https = require('https');
+const fs = require('fs');
 
+
+// FUNCION QUE RETORNA UNA PROMESA CON LOS DATOS DE LA API
 function request(url){
-    https
-    .get(url, (resp) => {
-        let data = '';
-        resp.on('data', (chunk) => {
-            data += chunk;
-            // console.log('chunk: ' + chunk);
-        });
-        resp.on('end', () => {
-            // console.log('data:', JSON.parse(data));
-            return JSON.parse(data)
+    return new Promise((resolve) => {
+        https
+        .get(url, (resp) => {
+            let data = '';
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+            resp.on('end', () => {
+                // RETORNAMOS LA RESOLUCION DE LA PROMESA
+                resolve(JSON.parse(data))
+            });
+        })
+        .on('error', (err) => {
+            console.log('Error: ' + err.message)
         });
     })
-    .on('error', (err) => {
-        console.log('Error: ' + err.message)
-    });
 }
 
 
 async function input(fileName, fileExt , money, qPesos){
-    // console.log('file_name', fileName)
-    // console.log('file_ext', fileExt)
-    // console.log('money', money)
-    // console.log('qPesos', qPesos)
+
+    // VALIDAR DATOS DE ENTRADA
+    if (!(fileName) || !(fileExt) || !(money) || !(qPesos)){
+        throw "No vienen todos los datos"
+    }
+
+
+    // TRAER DATOS DE LA API EN LA VARIABLE DATA
     const data = await request('https://mindicador.cl/api')
     // buscar la moneda en concreto 
-    console.log(data)
+    const moneyObj = data[money]
+    // VALIDAMOS LA EXISTENCIA DE LA MONEDA EN LA API
+    if (moneyObj){
+        moneyObj['file_name'] = fileName
+        moneyObj['file_ext'] = fileExt
+        moneyObj['qpesos'] = parseInt(qPesos)
+        console.log(moneyObj)
+
+        // CREAR EL ARCHIVO CON LA SALIDA CORRESPONDIENTE
+
+        // A la fecha: Thu Sep 03 2020 18:41:00 GMT-0400 (GMT-04:00) 
+        // Fue realizada cotización con los siguientes datos: 
+        // Cantidad de pesos a convertir: 250000 pesos 
+        // Convertido a "dólar" da un total de: 
+        //  $324,06
+
+        let result = moneyObj.qpesos / moneyObj.valor  // OPERACION DE CONVERTIR PESOS A MONEDA X
+        
+        let cadena = `A la fecha: ${moneyObj.fecha} ${'\n'}Fue realizada cotización con los siguientes datos: ${'\n'}Valor ${moneyObj.codigo}: ${ moneyObj.valor}${'\n'}Cantidad de pesos a convertir: ${moneyObj.qpesos} pesos${'\n'}Convertido a ${moneyObj.codigo} da un total de:${'\n'} ${result.toFixed(2)}`
+
+        // ESCRIBIMOS EN EL ARCHIVO
+        fs.writeFile(fileName + "." + fileExt, cadena , 'utf8', () => {
+            console.log('Archivo creado con éxito');
+            // AQUI QUEDAMOS
+            // A TRAVES DE CHILDPROCESS LEER EL ARCHIVO CREADO
+        });
+
+    }else{
+        console.log('Moneda no Encontrada')
+    }
+
 }
 
 let argument = process.argv
